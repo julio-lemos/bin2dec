dadfc7935f0a18a94afe515b55b1d339068eb138
Julio Lemos <juliolemos_@outlook.com>
2021-08-24 23:41:41 -0300

    feat: create main file to electron

---
+++ src/main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 500,
+    // resizable: false,
+      });
+
+  // win.removeMenu()
+  win.loadFile('../public/index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
